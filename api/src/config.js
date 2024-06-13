add929dc1f384936553bde05ee18e699673d266a
ArturoRM22 <A01643269@tec.mx>
2024-06-13 13:49:55 -0600

    Configured docker

--- api/src/config.js
+++ api/src/config.js
@@ -9,3 +9,4 @@ export const HOST = process.env.HOST || 'localhost';
 export const DATABASE = process.env.DATABASE || 'movicious';
 export const DB_PORT = process.env.DB_PORT || '3306';
 export const ACCESS_TOKEN_AUTH = process.env.ACCESS_TOKEN_AUTH;
+export const DB_DOCKER_HOST = process.env.DB_DOCKER_HOST;
